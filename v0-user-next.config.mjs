e873385c6207b693611fec8c6bf26cac71fd5042
nghia <86992802+nghiaph1706@users.noreply.github.com>
2025-03-19 10:56:52 +0700

    Update v0-user-next.config.mjs

--- v0-user-next.config.mjs
+++ v0-user-next.config.mjs
@@ -3,16 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      'nghiaph.id.vn',
-      'res.cloudinary.com',
-      'localhost',
-      '127.0.0.1',
-      '127.0.0.1:8000',
-      'maps.googleapis.com',
-      's3.amazonaws.com',
-      'pixarlaravel.s3.ap-southeast-1.amazonaws.com',
-      'pickbazarlaravel.s3.ap-southeast-1.amazonaws.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nghiaph.id.vn',
+      },
     ],
   },
   env: {
