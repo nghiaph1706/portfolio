d7c137b2aa9a116ac9c7560708b6f6ee61590ab4
nghia <86992802+nghiaph1706@users.noreply.github.com>
2025-03-19 10:45:09 +0700

    Update v0-user-next.config.mjs

--- v0-user-next.config.mjs
+++ v0-user-next.config.mjs
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'localhost',
+      '127.0.0.1',
+      '127.0.0.1:8000',
+      'maps.googleapis.com',
+      's3.amazonaws.com',
+      'pixarlaravel.s3.ap-southeast-1.amazonaws.com',
+      'pickbazarlaravel.s3.ap-southeast-1.amazonaws.com',
+    ],
+  },
   env: {
     PORT: "4200",
   },
